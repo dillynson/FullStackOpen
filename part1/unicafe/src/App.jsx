ce1a974644f89c87e341e97fcd17fc17d727fbc0
Dillyn Son <117866717+dillynson@users.noreply.github.com>
2026-09-16 19:16:36 -0700

    exercise 1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,19 +5,23 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setTotal(total + 1)
     console.log('good', good)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setTotal(total + 1)
     console.log('neutral', neutral)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setTotal(total + 1)
     console.log('bad', bad)
   }
 
@@ -33,6 +37,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {(good - bad)/total}</p>
+      <p>positive {(good/total) * 100}%</p>
     </div>
   )
 }
